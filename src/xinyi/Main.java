090624ce2407afaef08a55ff738048c6446c281b
xinyi524 <50417949+xinyi524@users.noreply.github.com>
2019-09-16 18:45:55 +0800

    Initial commit

---
+++ src/xinyi/Main.java
@@ -0,0 +1,12 @@
+package xinyi;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
